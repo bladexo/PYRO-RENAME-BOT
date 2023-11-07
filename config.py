dca2aedfcf9a994b911cd18f0ca40aa1fd047cd4
blade_xo <89949214+bladexo@users.noreply.github.com>
2023-11-07 23:57:32 +0100

    Update config.py

--- config.py
+++ config.py
@@ -26,20 +26,20 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")
-    API_HASH  = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID    = os.environ.get("API_ID", "25803426")
+    API_HASH  = os.environ.get("API_HASH", "291b6bea4848d7606c0d3213c317b430")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6918704487:AAHqzEF292uKEmUlzOv4F2n-OQQgE7y8AHU") 
    
     # database config
     DB_NAME = os.environ.get("DB_NAME","pyro-botz")     
-    DB_URL  = os.environ.get("DB_URL","")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://dinno001:brown@cluster0.u05dpzl.mongodb.net/?retryWrites=true&w=majority")
  
     # other configs
     BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
+    START_PIC   = os.environ.get("START_PIC", "https://graph.org/file/d6ea003ee0a05d0636302.jpg")
     ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "RedOfficiall") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "musicdownloaderr"))
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
